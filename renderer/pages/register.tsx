845f3483d6c64217bf62a1e61747e43e64991c34
so0112 <tkddh0112@gmail.com>
2022-12-28 02:38:46 +0900

    Feat: 회원가입 페이지 레이아웃

--- renderer/pages/register.tsx
+++ renderer/pages/register.tsx
@@ -1,9 +1,12 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import Link from "next/link";
+import { useRef } from "react";
 
 type Inputs = {
-  example: string;
-  exampleRequired: string;
+  email: string;
+  nickname: string;
+  password: string;
+  passwordCheck: string;
 };
 
 export default function App() {
@@ -12,46 +15,95 @@ export default function App() {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Inputs>();
+  } = useForm<Inputs>({ mode: "onChange" });
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const password = useRef();
+  password.current = watch("password");
 
-  console.log(watch("example")); // watch input value by passing the name of it
+  console.log(watch("email"));
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen p-6">
-      <div className="w-10/12 mx-auto md:w-96">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="m-auto">
         <h1 className="mb-2 text-lg font-medium">회원가입</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <input
-              className="mb-1 border-black border-2"
-              defaultValue=""
-              {...register("example")}
-            />
-          </div>
+        <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            placeholder="이메일"
+            className="w-60 mb-2 border-black border-2 p-1"
+            defaultValue=""
+            type={"email"}
+            {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+          />
 
-          <div>
-            <input
-              className="mb-1 border-black border-2"
-              {...register("exampleRequired", { required: true })}
-            />
-          </div>
-          {errors.exampleRequired && (
-            <div>
-              <small className="font-medium text-red-500">값을 넣어라</small>
-            </div>
+          {errors.email && (
+            <small className="">올바른 이메일을 입력해주세요</small>
           )}
 
           <input
-            className="w-20 py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded"
+            placeholder="닉네임"
+            className="w-60 mb-2 border-black border-2 p-1"
+            {...register("nickname", { required: true, maxLength: 10 })}
+          />
+
+          {errors.nickname && errors.nickname.type === "required" && (
+            <small className="">닉네임을 입력해주세요</small>
+          )}
+
+          {errors.nickname && errors.nickname.type === "maxLength" && (
+            <small className="">10자 이내의 닉네임을 사용해주세요</small>
+          )}
+
+          <input
+            placeholder="비밀번호"
+            className="w-60 mb-2 border-black border-2 p-1"
+            type={"password"}
+            {...register("password", { required: true, minLength: 8 })}
+          />
+
+          {errors.password && errors.password.type === "required" && (
+            <small className="">비밀번호를 입력해주세요</small>
+          )}
+
+          {errors.password && errors.password.type === "minLength" && (
+            <small className="">비밀번호는 최소 8자 이상입니다</small>
+          )}
+
+          <input
+            placeholder="비밀번호 확인"
+            className="w-60 mb-2 border-black border-2 p-1"
+            type={"password"}
+            {...register("passwordCheck", {
+              required: true,
+              validate: (value) => value === password.current,
+            })}
+          />
+
+          {errors.passwordCheck && errors.passwordCheck.type === "required" && (
+            <small className="">비밀번호 확인을 입력해주세요</small>
+          )}
+
+          {errors.passwordCheck && errors.passwordCheck.type === "validate" && (
+            <small className="">비밀번호가 다릅니다</small>
+          )}
+
+          {/* {errors.exampleRequired && (
+            <div>
+              <small className="font-medium text-red-500">
+                input 값을 모두 채워주세요
+              </small>
+            </div>
+          )} */}
+
+          <input
+            className="w-60 py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded"
             type="submit"
           />
         </form>
 
         <small>
-          아이디가 없으신가요?
+          아이디가 있으신가요?
           <Link href="/login">
-            <a className="ml-1 text-blue-500 uppercase">로그인</a>
+            <a className="ml-1 text-blue-500">로그인</a>
           </Link>
         </small>
       </div>
